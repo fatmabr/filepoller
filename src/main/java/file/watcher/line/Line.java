d1fd3407ea9ed19fd170a12f39fcb76691326bdd
fatma <laila1983@>
2017-07-09 14:49:24 +0200

    refactor sample processor into a separated module

--- src/main/java/file/watcher/line/Line.java
+++ src/main/java/file/watcher/line/Line.java
@@ -3,6 +3,6 @@ package file.watcher.line;
 /**
  * Created by bradai on 28/06/2017.
  */
-public abstract class Line {
-    //public abstract Line newLine(String[] line);
+public interface Line {
+
 }
